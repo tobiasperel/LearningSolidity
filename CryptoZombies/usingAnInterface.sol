ebdd0a540d27dd4caed32b7b32a94b0cc51b2f18
TobiPerel <pereltobias@gmail.com>
2022-09-17 16:03:00 -0300

    finish lesson 2

---
+++ CryptoZombies/usingAnInterface.sol
@@ -0,0 +1,18 @@
+// Con la siguente interface:
+
+contract NumberInterface {
+  function getNum(address _myAddress) public view returns (uint);
+}
+//We can use it in a contract as follows:
+contract MyContract {
+  address NumberInterfaceAddress = 0xab38... 
+  // ^ The address of the FavoriteNumber contract on Ethereum
+  NumberInterface numberContract = NumberInterface(NumberInterfaceAddress);
+  // Now `numberContract` is pointing to the other contract
+
+  function someFunction() public {
+    // Now we can call `getNum` from that contract:
+    uint num = numberContract.getNum(msg.sender);
+    // ...and do something with `num` here
+  }
+}
