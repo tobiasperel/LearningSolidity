d5bb059d988b4ac6df8453d67dfb0670c62bf67d
tobiasperel <74215065+tobiasperel@users.noreply.github.com>
2022-09-20 11:04:35 -0300

    Create importMultipleFiles.sol

---
+++ CryptoZombies/importMultipleFiles.sol
@@ -0,0 +1,8 @@
+contract SatoshiNakamoto is NickSzabo, HalFinney {
+  // Omg, the secrets of the universe revealed!
+}
+
+/*
+As you can see, when using multiple inheritance, you just separate the multiple contracts you're inheriting from with a comma, ,. 
+In this case, our contract is inheriting from NickSzabo and HalFinney.
+*/
