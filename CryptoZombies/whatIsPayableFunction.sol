28b96e47054a6890a6bfebf6470e2b3a5f703e70
TobiPerel <pereltobias@gmail.com>
2022-09-19 22:42:44 -0300

    update code

--- CryptoZombies/whatIsPayableFunction.sol
+++ CryptoZombies/whatIsPayableFunction.sol
@@ -12,3 +12,21 @@ a function and pay money to the contract at the same time.
 This allows for some really interesting logic, like requiring a certain payment
 to the contract in order to execute a function.
 */
+contract OnlineStore {
+  function buySomething() external payable {
+    // Check to make sure 0.001 ether was sent to the function call:
+    require(msg.value == 0.001 ether);
+    // If so, some logic to transfer the digital item to the caller of the function:
+    transferThing(msg.sender);
+  }
+}
+
+//Here, msg.value is a way to see how much Ether was sent to the contract, and ether is a built-in unit.
+
+/* What happens here is that someone would call the function from web3.js
+ (from the DApp's JavaScript front-end) as follows: */
+
+// Assuming `OnlineStore` points to your contract on Ethereum:
+OnlineStore.buySomething({from: web3.eth.defaultAccount, value: web3.utils.toWei(0.001)})
+
+
